124dae783234dade6c24b1f105f251c5b218f327
Jannis Pohlmann <contact@jannispohlmann.de>
2017-12-11 16:50:48 +0100

    Playground: Fix layout issues in results view

--- packages/graphql-playground/src/components/Playground/Results.tsx
+++ packages/graphql-playground/src/components/Playground/Results.tsx
@@ -48,11 +48,6 @@ export default class Results extends React.Component<Props, {}> {
           }
 
           .result-viewer-wrapper {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
             overflow: auto;
           }
         `}</style>
